f5f00b6eec5e3d4ca968db3700ba98560f56abbf
consr2 <qqq4861@naver.com>
2022-11-24 16:15:36 +0900

    메인에 타겟 추가함

--- Funding/src/main/java/com/funding/HomeController.java
+++ Funding/src/main/java/com/funding/HomeController.java
@@ -3,12 +3,15 @@ package com.funding;
 import java.security.Principal;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.funding.fundArtist.FundArtist;
 import com.funding.fundArtist.FundArtistService;
+import com.funding.fundBoardTarget.FundBoardTarget;
+import com.funding.fundBoardTarget.FundTargetService;
 import com.funding.fundUser.FundUser;
 import com.funding.fundUser.FundUserService;
 
@@ -20,27 +23,28 @@ public class HomeController {
 	
 	private final FundUserService fundUserService;
 	private final FundArtistService fundArtistService;
+	private final FundTargetService fundTargetService;
 	
 	// 메인페이지 요청시 로그인된 사용자 정보를 같이 넘겨준다
 	@RequestMapping("/")
 	public String home(Model model, Principal principal) {
 		
 	
-	 if(principal != null) {
-		 Optional<FundUser> fundUser =  this.fundUserService.findByuserName(principal.getName());
-		 Optional<FundArtist> fundArtist = this.fundArtistService.findByuserName(principal.getName());
+		if(principal != null) {
+			Optional<FundUser> fundUser =  this.fundUserService.findByuserName(principal.getName());
+			Optional<FundArtist> fundArtist = this.fundArtistService.findByuserName(principal.getName());
+			 
+			if(fundUser.isPresent()) {
+				model.addAttribute("userData",fundUser.get());
+			}
 		 
-		 if(fundUser.isPresent()) {
-			 model.addAttribute("userData",fundUser.get());
-		 }
-		 
-		 if(fundArtist.isPresent()) {
-			 model.addAttribute("userData",fundArtist.get());
-		 }
-	 }
-	 
-	 
-	 
+			if(fundArtist.isPresent()) {
+				model.addAttribute("userData",fundArtist.get());
+			}
+		}
+		Page<FundBoardTarget> page = fundTargetService.findAll(0); 
+		
+		model.addAttribute("page", page);
 		return "main/home";
 	}
 }
