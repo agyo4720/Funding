e734956f967307e79b0394d6785d997cefdb2d65
startyuphoney <96329521+startyuphoney@users.noreply.github.com>
2022-11-24 09:51:47 +0900

    로그인 로그아웃 최신
    ㄱㄱㄱ

--- Funding/src/main/java/com/funding/HomeController.java
+++ Funding/src/main/java/com/funding/HomeController.java
@@ -25,16 +25,16 @@ public class HomeController {
 	@RequestMapping("/")
 	public String home(Model model, Principal principal) {
 		
-	 Optional<FundUser> fundUser =  this.fundUserService.findByuserName(principal.getName());
-	 Optional<FundArtist> fundArtist = this.fundArtistService.findByuserName(principal.getName());
-	 
-	 if(fundUser.isPresent()) {
-		 model.addAttribute("userData",fundUser);
-	 }
-	 
-	 if(fundArtist.isPresent()) {
-		 model.addAttribute("userData",fundArtist);
-	 }
+//	 Optional<FundUser> fundUser =  this.fundUserService.findByuserName(principal.getName());
+//	 Optional<FundArtist> fundArtist = this.fundArtistService.findByuserName(principal.getName());
+//	 
+//	 if(fundUser.isPresent()) {
+//		 model.addAttribute("userData",fundUser);
+//	 }
+//	 
+//	 if(fundArtist.isPresent()) {
+//		 model.addAttribute("userData",fundArtist);
+//	 }
 	 
 	 
 		return "main/home";
