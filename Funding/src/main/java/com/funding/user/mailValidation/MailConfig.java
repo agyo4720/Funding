1caad6efe5d72365da33d3dabbefcdd54bffd69f
startyuphoney <96329521+startyuphoney@users.noreply.github.com>
2022-11-28 16:17:20 +0900

    rr
    rr

--- Funding/src/main/java/com/funding/user/mailValidation/MailConfig.java
+++ Funding/src/main/java/com/funding/user/mailValidation/MailConfig.java
@@ -15,8 +15,8 @@ public class MailConfig {
 		JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 		
 		javaMailSender.setHost("smtp.naver.com");
-		javaMailSender.setUsername("네이버 아이디");
-		javaMailSender.setPassword("비밀번호");
+		javaMailSender.setUsername("네이버아이디");
+		javaMailSender.setPassword("네이버비밀번호");
 		
 		javaMailSender.setPort(465);
 		javaMailSender.setJavaMailProperties(getMailProperties());
