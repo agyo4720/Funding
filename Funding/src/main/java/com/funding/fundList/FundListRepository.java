d7a3ab279998eed746810bce64828ba5348a1f51
qor8005 <107646818+qor8005@users.noreply.github.com>
2022-12-01 17:40:30 +0900

    pay(12-01(5))

--- Funding/src/main/java/com/funding/fundList/FundListRepository.java
+++ Funding/src/main/java/com/funding/fundList/FundListRepository.java
@@ -1,7 +1,15 @@
 package com.funding.fundList;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FundListRepository extends JpaRepository<FundList, Integer>{
+import com.funding.fundBoard.FundBoard;
+import com.funding.fundBoardTarget.FundBoardTarget;
+import com.funding.fundTargetList.FundTargetList;
+import com.funding.fundUser.FundUser;
 
+public interface FundListRepository extends JpaRepository<FundList, Integer>{
+	public List<FundList> findByFundBoard(FundBoard fundBoard);
+	public List<FundList> findByFundUserAndFundBoard(FundUser funduser, FundBoard fundBoard);
 }
--- Funding/src/main/java/com/funding/fundList/FundListService.java
+++ Funding/src/main/java/com/funding/fundList/FundListService.java
@@ -1,8 +1,16 @@
 package com.funding.fundList;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+
+import com.funding.fundBoard.FundBoard;
+import com.funding.fundBoardTarget.FundBoardTarget;
+import com.funding.fundTargetList.FundTargetList;
+import com.funding.fundUser.FundUser;
+import com.funding.fundUser.FundUserService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,10 +19,34 @@ import lombok.RequiredArgsConstructor;
 public class FundListService {
 
 	private final FundListRepository fundListRepository;
+	private final FundUserService fundUserService;
 	
 	public List<FundList> getFundList(){
 		return this.fundListRepository.findAll();
 	}
 	
 	
+	//지정펀딩 펀딩하면 db에 등록
+	public void insertList(Principal principal, FundBoard fundBoard) {
+		Optional<FundUser> user = fundUserService.findByuserName(principal.getName());
+		
+		FundList fundBoardList = new FundList();
+		fundBoardList.setFundBoard(fundBoard);
+		fundBoardList.setFundUser(user.get());
+		
+		fundListRepository.save(fundBoardList);
+	}
+	
+	
+	//펀드글로 리스트 부르기
+	public List<FundList> findByFundBoard(FundBoard fundBoard){
+		List<FundList> fList = fundListRepository.findByFundBoard(fundBoard);
+		return fList;
+	}
+	
+	//해당 리스트 지우기
+	public void delete(FundUser user, FundBoard fundBoard) {
+		List<FundList> fList = fundListRepository.findByFundUserAndFundBoard(user, fundBoard);
+		fundListRepository.delete(fList.get(0));
+	}
 }
